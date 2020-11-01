b31147e18405e9e0b9d84494b88f35ba6ef25631
Marco <marco.chianese93@gmail.com>
2020-11-01 18:07:20 +0100

    esercizio completo (in parte)

--- script.js
+++ script.js
@@ -12,23 +12,48 @@
 
 $(document).ready(function () {
 
-
 // ho iniziato a dichiarare le variabili quella della lista dei numeri random ed il numero in se
 var numero_random = 0 ;
 var lista_numeri = [] ;
-
-
+var numeri_utente = [];
+var scelta_utente ;
+var numeri_indovinati = 0;
+var lista_indovinati = [] ;
 // in questo ciclo stampo 5 numeri random e li vado ad aggiungere all'array della lista dei numeri trovati
 while (lista_numeri.length < 5) {
     numero_random = getRndInteger(1 , 100)
     lista_numeri.push(numero_random);
     console.log(lista_numeri);
 }
- $(".lista_numeri").html('<p> ' + lista_numeri + ' </p>');
+ $(".lista_numeri").text(lista_numeri);
 
+//dopo 3 secondi i numeri spariranno dallo schermo
 setTimeout(function( ) {
     $(".lista_numeri").removeClass("active")
 }  , 3000);
+
+setInterval(function() {
+    while (numeri_utente.length < 5) {
+        scelta_utente = (parseInt(prompt("scrivi un numero"))) ;
+        if (numeri_utente.includes(scelta_utente)) {
+            alert("Numero giá selezionato!!!scrivi un numero") ;
+        } else {
+            numeri_utente.push(scelta_utente);
+            console.log(numeri_utente);
+        }
+
+        if (lista_numeri.includes(scelta_utente)) {
+            numeri_indovinati++ ;
+            lista_indovinati.push(scelta_utente)
+        }
+
+
+        console.log("QUesto é il numero di volte che hai indovinato " + numeri_indovinati);
+        console.log(" questi sono i numeri che hai indovinato " + lista_indovinati);
+
+}}, 3000);
+
+
 
 
  function getRndInteger(min,max) {
